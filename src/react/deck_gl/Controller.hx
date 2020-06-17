d9f44c72dfb05a976beecf6b3aec72727bf4ae5e
Kevin Leung <kevinresol@gmail.com>
2020-06-17 14:33:42 +0800

    Add Controller

---
+++ src/react/deck_gl/Controller.hx
@@ -0,0 +1,11 @@
+package react.deck_gl;
+
+typedef Controller = {
+	?scrollZoom:Bool,
+	?dragPan:Bool,
+	?dragRotate:Bool,
+	?doubleClickZoom:Bool,
+	?touchZoom:Bool,
+	?touchRotate:Bool,
+	?keyboard:Bool,
+}
--- src/react/deck_gl/Deck.hx
+++ src/react/deck_gl/Deck.hx
@@ -2,15 +2,17 @@ package react.deck_gl;
 
 import react.ReactComponent;
 import react.map_gl.ViewState;
+import haxe.extern.EitherType;
 
 @:jsRequire('@deck.gl/react', 'default')
 extern class Deck extends ReactComponentOfProps<DeckProps> {}
 
 typedef DeckProps = {
 	?viewState:ViewState,
-	?controller:Bool,
+	?controller:EitherType<Controller, Bool>,
 	?layers:Array<Layer<{}>>,
 	?children:ReactFragment,
 	?onViewStateChange:{viewState:ViewState}->Void,
 	?onClick:PickingInfo<{}>->Dynamic->Void,
 }
+
