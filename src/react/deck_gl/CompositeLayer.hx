e43d70f3025de427d0926b8b66c4a8109c8bc126
Kevin Leung <kevinresol@gmail.com>
2021-10-22 17:11:48 +0800

    Fix

--- src/react/deck_gl/CompositeLayer.hx
+++ src/react/deck_gl/CompositeLayer.hx
@@ -4,9 +4,9 @@ import haxe.Constraints;
 import react.deck_gl.Layer;
 
 @:jsRequire('@deck.gl/core', 'CompositeLayer')
-extern class CompositeLayer<Props:{}> extends Layer<Props> {
-	function renderLayers():Array<Layer<{}>>;
-	function getPickingInfo(pickParams:{info:PickInfo, mode:String, sourceLayer:Layer<{}>}):PickingInfo<{}>;
+extern class CompositeLayer<Props:BasicPropsWithoutData> extends Layer<Props> {
+	function renderLayers():Array<Layer<BasicPropsWithoutData>>;
+	function getPickingInfo(pickParams:{info:PickInfo, mode:String, sourceLayer:Layer<BasicPropsWithoutData>}):PickingInfo<BasicPropsWithoutData>;
 	function getSubLayerProps<Out>(subLayerProps:Out):Out;
 	function shouldRenderSubLayer<Data>(id:String, data:Data):Bool;
 	function getSubLayerClass(id:String, DefaultLayerClass:Any):Function;
